163c6f50aa198d8fdf7e6e3da41cb98b314a1eb9
soumya <soumya.chakraborty@hoichoi.tv>
2024-04-11 16:28:29 +0530

    to use jsx, component composition etc

--- App.js
+++ App.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-const parent = React.createElement('h1', {}, 'Ignition of an Application.');
-
+// React Element
+const heading = React.createElement('h1', { id: 'heading' }, 'Hello world!');
+// jsx
+const header = <h1 className="header">Hello there!</h1>;
+// React Component
+const HeaderComponent = () => (
+	<div>
+		{header}
+		<h1>This is a Header component.</h1>
+	</div>
+);
 const root = ReactDOM.createRoot(document.getElementById('root'));
-
-root.render(parent);
+console.log(header, 'h1', heading, 'h3');
+root.render(<HeaderComponent />);
